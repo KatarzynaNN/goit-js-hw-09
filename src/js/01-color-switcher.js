449ec1470837c42d311b3500971cbeb8c920cf51
KatarzynaNN <kat.nedza@gmail.com>
2023-09-22 10:06:56 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startColorChange = () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled');
+};
+
+const stopColorChange = () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+};
+
+startBtn.addEventListener('click', startColorChange);
+
+stopBtn.addEventListener('click', stopColorChange);
